a06819760a41e86a12652230e7442ca3c27abc93
Nayansingh17ab <98090891+Nayansingh17ab@users.noreply.github.com>
2026-09-13 08:19:50 +0530

    ✅ Weather Observation Station 7 [db2] — approach1

---
+++ solutions/db2/easy/weather-observation-station-7/solution-approach1.sql
@@ -0,0 +1,25 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-7/problem?isFullScreen=true
+-- Problem     Weather Observation Station 7
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    db2
+-- Status      Accepted
+-- Submitted   2026-09-13, 08:19 a.m.
+-- ──────────────────────────────────────────────────
+
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+
+SELECT DISTINCT CITY FROM STATION
+WHERE LOWER(CITY) LIKE '%a'
+OR LOWER(CITY) LIKE '%e'
+OR  LOWER(CITY) LIKE '%i'
+OR  LOWER(CITY) LIKE '%o'
+OR  LOWER(CITY) LIKE '%u';
