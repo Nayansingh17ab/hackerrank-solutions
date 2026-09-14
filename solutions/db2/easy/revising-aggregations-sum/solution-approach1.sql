1c44ccd210cb9e9523f16aff40f89eec9af464c6
Nayansingh17ab <98090891+Nayansingh17ab@users.noreply.github.com>
2026-09-14 15:09:42 +0530

    ✅ Revising Aggregations - The Sum Function [db2] — approach1

---
+++ solutions/db2/easy/revising-aggregations-sum/solution-approach1.sql
@@ -0,0 +1,21 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/revising-aggregations-sum/problem?isFullScreen=true
+-- Problem     Revising Aggregations - The Sum Function
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    db2
+-- Status      Accepted
+-- Submitted   2026-09-14, 03:09 p.m.
+-- ──────────────────────────────────────────────────
+
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/ 
+SELECT SUM(POPULATION) FROM CITY
+GROUP BY DISTRICT
+HAVING DISTRICT = 'California';
