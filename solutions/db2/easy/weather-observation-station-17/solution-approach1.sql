9071efe61dccbc1caf86a71abbe45d932a2c6b07
Nayansingh17ab <98090891+Nayansingh17ab@users.noreply.github.com>
2026-09-14 14:58:37 +0530

    ✅ Weather Observation Station 17 [db2] — approach1

---
+++ solutions/db2/easy/weather-observation-station-17/solution-approach1.sql
@@ -0,0 +1,23 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-17/problem?isFullScreen=true
+-- Problem     Weather Observation Station 17
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    db2
+-- Status      Accepted
+-- Submitted   2026-09-14, 02:58 p.m.
+-- ──────────────────────────────────────────────────
+
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+
+SELECT CAST(ROUND(LONG_W,4) AS DECIMAL (12,4))
+FROM STATION 
+WHERE LAT_N>38.7780
+ORDER BY LAT_N LIMIT 1;
